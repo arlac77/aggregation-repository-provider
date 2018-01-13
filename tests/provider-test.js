004ee01aee1d578f2b44df38cf060ad084876f95
Markus Felten <Markus.Felten@gmx.de>
2018-01-13 16:23:13 +0100

    feat: repository(name) skeleton implementatnion

--- tests/provider-test.js
+++ tests/provider-test.js
@@ -1,10 +1,10 @@
 import test from 'ava';
-import { GithubRepositoryProvider } from 'github-repository-provider';
+import { GithubProvider } from 'github-repository-provider';
 
 import { AggregationProvider } from '../src/repository-provider';
 
 test('locate github', async t => {
-  const provider = new AggregationProvider([new GithubRepositoryProvider()]);
+  const provider = new AggregationProvider([new GithubProvider()]);
 
   const repository = await provider.repository(
     'https://github.com/arlac77/aggregation-repository-provider.git'
