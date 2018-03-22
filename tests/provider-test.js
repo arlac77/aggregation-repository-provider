19cb9c77066ca6291b945e3ef3397b7b78b0c4e9
Markus Felten <Markus.Felten@gmx.de>
2018-03-22 07:32:27 +0100

    test: check git@github.com too

--- tests/provider-test.js
+++ tests/provider-test.js
@@ -4,7 +4,7 @@ import { BitbucketProvider } from 'bitbucket-repository-provider';
 import { LocalProvider } from 'local-repository-provider';
 import { AggregationProvider } from '../src/repository-provider';
 
-test('locate github', async t => {
+test('locate github https', async t => {
   const provider = new AggregationProvider([
     new GithubProvider(),
     new BitbucketProvider(),
@@ -16,6 +16,24 @@ test('locate github', async t => {
   );
 
   t.is(repository.name, 'arlac77/aggregation-repository-provider');
+});
+
+test('locate github git@', async t => {
+  if (process.env.SSH_AUTH_SOCK) {
+    const provider = new AggregationProvider([
+      new GithubProvider(),
+      new BitbucketProvider(),
+      new LocalProvider()
+    ]);
+
+    const repository = await provider.repository(
+      'git@github.com:arlac77/aggregation-repository-provider.git'
+    );
+
+    t.is(repository.name, 'arlac77/aggregation-repository-provider');
+  } else {
+    t.is(1, 1, 'skip git@ test without SSH_AUTH_SOCK');
+  }
 });
 
 test('locate github short', async t => {
