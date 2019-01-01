21b48b19feeec872da1184bebc126307df307ad5
Markus Felten <Markus.Felten@gmx.de>
2019-01-01 22:28:59 +0100

    fix(rollup): don´t thread repository-provider as external

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
 import json from "rollup-plugin-json";
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import executable from "rollup-plugin-executable";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import pkg from "./package.json";
 
 export default {
   input: pkg.module,
   output: {
     file: pkg.main,
-    format: 'cjs',
+    format: "cjs",
     interop: false
   },
   plugins: [resolve(), commonjs(), cleanup()],
-  external: ['repository-provider']
+  external: []
 };
